a3cb9e5ec1b1a2dcbfb832fe7c4b8f2760779f49
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-03-19 19:02:07 -0500

    ??

--- MovieJson/ViewController.swift
+++ MovieJson/ViewController.swift
@@ -1,20 +1,56 @@
 //
 //  ViewController.swift
-//  MovieJson
+//  JSON
 //
-//  Created by Samantha Cortopassi on 3/19/19.
+//  Created by Samantha Cortopassi on 2/13/19.
 //  Copyright © 2019 Samantha Cortopassi. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+struct WebsiteDescription: Decodable {
+    let kind: String?
+    let Listing: String?
+    let data: [Course]?
+}
 
+struct Course: Decodable {
+    let kind: Int
+    let Listing: String
+    let data: String
+}
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://www.reddit.com/r/marvelstudios.json"
+        
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            
+            guard let data = data else { return }
+            
+            //            let dataAsString = String(data: data, encoding: .utf8)
+            //            print(dataAsString)
+            
+            do {
+                let courses = try JSONDecoder().decode([WebsiteDescription].self, from: data)
+                print(courses)
+                
+                //  2/3/objC
+                //                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+                //
+                //                let course = Course(json: json)
+                //                print(course.name)
+                
+            } catch let jsonErr {
+                print("serializing error", jsonErr)
+            }
+            
+            }.resume()
     }
-
-
 }
 
